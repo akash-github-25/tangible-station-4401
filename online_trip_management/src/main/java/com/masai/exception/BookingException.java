082a676f03cd3a719383d8f2cb197bb9b0da6989
Rishon-A-Singh <rishon.singh@gmail.com>
2022-11-10 17:53:48 +0530

    day 2 rishon

---
+++ online_trip_management/src/main/java/com/masai/exception/BookingException.java
@@ -0,0 +1,18 @@
+package com.masai.exception;
+
+public class BookingException extends Exception{
+	
+public BookingException() {
+	// TODO Auto-generated constructor stub
+}
+
+public BookingException(String message) {
+	super(message);
+	// TODO Auto-generated constructor stub
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
