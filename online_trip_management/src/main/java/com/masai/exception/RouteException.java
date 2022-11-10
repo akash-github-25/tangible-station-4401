4062e750c14928e2e283ec5cdd3a5ceff86abf0e
Rishon-A-Singh <rishon.singh@gmail.com>
2022-11-10 18:18:04 +0530

    day3 rishon

---
+++ online_trip_management/src/main/java/com/masai/exception/RouteException.java
@@ -0,0 +1,15 @@
+package com.masai.exception;
+
+public class RouteException extends Exception{
+	
+public RouteException() {
+	// TODO Auto-generated constructor stub
+}
+
+public RouteException(String message) {
+	super(message);
+	// TODO Auto-generated constructor stub
+}
+	
+
+}
---
+++ online_trip_management/src/main/java/com/masai/exception/TravelsException.java
@@ -0,0 +1,13 @@
+package com.masai.exception;
+
+public class TravelsException extends Exception{
+	
+public TravelsException() {
+	// TODO Auto-generated constructor stub
+}
+public TravelsException(String message) {
+	super(message);
+	// TODO Auto-generated constructor stub
+}
+	
+}
